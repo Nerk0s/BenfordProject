db0de0a5679b2d74f05edf1efa9fe0fef625de50
Nerk0s <83953324+Nerk0s@users.noreply.github.com>
2024-04-12 10:09:35 +0200

    Changed name to number_counter.py

--- Number_counter.py
+++
@@ -1,5 +0,0 @@
-def zahl_counter():
-    # This is a placeholder Code
-    print("Dieses Programm zählt von 1 bis 100.")
-    for i in range(1, 101):
-        print(i)
---
+++ number_counter.py
@@ -0,0 +1,15 @@
+"""
+this module has as purpose, to count all the digits from a single or a group of pdf files
+"""
+
+
+def zahl_counter():
+    """
+    This is a placeholder Code
+    """
+    print("Dieses Programm zählt von 1 bis 100.")
+    for i in range(1, 101):
+        print(i)
+
+
+zahl_counter()
