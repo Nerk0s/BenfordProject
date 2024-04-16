1a3b26ab10bdc35d058622625f5dfc3916cc3ab7
Nerk0s <83953324+Nerk0s@users.noreply.github.com>
2024-04-16 14:49:30 +0200

    changes Baby!!!

---
+++ install.cmd
@@ -0,0 +1,8 @@
+pip install PyPDF2
+
+pip install BenfordLaw
+
+set /P var123="Enter the path to the directory where you want to install the repo: "
+cd /d %var123%
+
+git clone https://github.com/Nerk0s/BenfordProject.git
